a4e63048b505cb332f5ea28c66710495544a1dbc
WassefRPZ <10422@holbertonstudents.com>
2025-12-12 16:16:01 +0100

    Refactor SkyHighBuilding class to use private floors property and improve evacuation warning message

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,22 +1,18 @@
-import Building from "./5-building";
+import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors){
-        super(sqft);
-        this.floors = floors;
-    }
-
-    get floors(){
-        return this._floors;
-    }
-    set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    this._floors = value;
-    }
-    
-    evacuationWarningMessage(){
-        return `Evacuate slowly the ${this.floors} floors.`;
-    }
+	constructor(sqft, floors) {
+		super(sqft);
+		this._floors = floors
+	}
+	//getters
+	get sqft() {
+		return this._sqft
+	}
+	get floors() {
+		return this._floors
+	}
+	evacuationWarningMessage() {
+		return `Evacuate slowly the ${this._floors} floors`
+	}
 }
