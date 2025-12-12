45dddbe20b282ff39f11a4cb3e2bc51b2118ad54
WassefRPZ <10422@holbertonstudents.com>
2025-12-12 16:17:23 +0100

    Refactor Building class to enforce evacuationWarningMessage override and streamline sqft property handling

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,11 @@
-export default class Building{
-    constructor(sqft){
-        this.sqft = sqft;
-    }
-    get sqft(){
-        return this._sqft;
-    }
-    set sqft(value){
-        if (typeof value !== "number"){
-            throw new TypeError("number Error");
-        }
-        this._sqft = value;
-    }
-    evacuationWarningMessage(){
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+export default class Building {
+	constructor(sqft) {
+		if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+			throw new Error('Class extending Building must override evacuationWarningMessage') }
+		this._sqft = sqft
+	}
+	//getter
+	get sqft() {
+		return this._sqft
+	}
 }
